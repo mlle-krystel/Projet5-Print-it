3a967e92c8df6ef205fe75c0e909a4a8e855bf09
Krystel Drappier <melle.krys@gmail.com>
2024-12-03 16:40:50 +0100

    evenlistener sur les flèches

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+// Sélection des flèches
+const precedent = document.getElementById("precedent");
+const suivant = document.getElementById("suivant");
+
+// Vérif
+console.log("Bouton Précédent :", precedent);
+console.log("Bouton Suivant :", suivant);
+
+
+// Gestionnaire d'événement pour "Précédent"
+precedent.addEventListener("click", function() {
+  index = (index - 1 + slides.length) % slides.length; 
+
+  console.log("Slide précédent, index :", index);
+});
+
+suivant.addEventListener("click", function() { index = (index + 1) % slides.length;
+console.log("slide suivant, index :", index)});
+
+
