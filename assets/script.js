faf539dd1cddfdfe5da8756bb0cbac1eb7e4d644
Krystel Drappier <melle.krys@gmail.com>
2024-12-05 22:35:48 +0100

    carroussel + dots actifs

--- assets/script.js
+++ assets/script.js
@@ -18,35 +18,75 @@ const slides = [
 	}
 ]
 
-// Initialisation de l'index
-let index = 0;
-console.log("Index initial : ", index);
+//Index globale pour indication du slide actif
+let indexCurrent = 0;
+console.log("Index initial : ", indexCurrent);
 
-// Sélection des flèches
-const precedent = document.getElementById("precedent");
+// appelle les elements
+const bannerImage = document.querySelector(".banner-img");
+const paragraphBanner = document.getElementById("paragraphBanner");
+const bulletsContainer = document.querySelector(".dots");
 const suivant = document.getElementById("suivant");
+const precedent = document.getElementById("precedent");
 
-// Vérif
-console.log("precedent :", precedent);
-console.log("suivant :", suivant);
+function activeSlide(){
+	// mise à jour du tagLine actif
+	paragraphBanner.innerHTML = slides[indexCurrent].tagLine;
+	// mise à jour du slide actif
+	bannerImage.src = `./assets/images/slideshow/${slides[indexCurrent].image}`;
 
+	// mise à jour du dot actif
+	// ne pas oublier d'appeler l'element dots
+	const dots = document.querySelectorAll(".dot");
+dots.forEach((dot, indexDots ) => {
+	if(indexDots === indexCurrent) {
+		dot.classList.add("dot_selected");
+	}
+	else {
+		dot.classList.remove("dot_selected");
+	}
+});
+}
 
-// Evénement pour "Précédent"
+// Evénement pour "Précédent" avec indice décrémenté -- manuellement
 precedent.addEventListener("click", function() {
-  index = (index - 1 + slides.length) % slides.length; 
-
-  console.log("Slide précédent, index :", index);
+  indexCurrent --;  
+//   si inf < à 0 alors indexCurrent recule de 1 (faire attention à la syntaxe !)
+  if (indexCurrent < 0) {
+	indexCurrent = slides.length -1;
+  }
+  console.log("Slide précédent, index :", indexCurrent);
+  activeSlide();
 });
 
-suivant.addEventListener("click", function() { index = (index + 1) % slides.length;
-console.log("slide suivant, index :", index)});
+// evenement pour suivant avec modulo % pour assurer le retour au 1er slide après le dernier
+suivant.addEventListener("click", function() { indexCurrent = (indexCurrent + 1) % slides.length;
+console.log("slide suivant, index :", indexCurrent) 
 
-// test pour dots
-const bullets = document.querySelector(".dots");
-bullets.classList.add("dot");
-console.log(bullets);
+// appel de la fonction pour activer le changement d'img
+activeSlide();
+});
 
-// creation de 4 dots
-numberDots = 4;
+
+// creation des dots pour qu'ils parcourt chaque slide
+slides.forEach(function(slide, indexSlide) {
+	const dot = document.createElement("span");
+	dot.classList.add("dot");
+	// attribution à la div .dots
+	bulletsContainer.appendChild(dot);
+
+	// evenement & association de l'index du dot à l'index du slide
+dot.addEventListener("click", function() {
+	// mise à jour l'index general (indexcurrent) avec le dot actif
+indexCurrent = indexSlide;
+activeSlide();
+
+
+// mise à jour de l'affichage // à l'index actif
+activeSlide();
+});
+});
+
+// creation de fonctions pour mise à jour du slide actif (img + tagLine)
 
 
